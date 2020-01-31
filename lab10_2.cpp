8194067c74ae084c3312f6a824cc9aa7080997b3
Dexshine <b_o_y_s@hotmail.com>
2020-01-31 16:17:36 +0700

    fix playerscore and yugiscore

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -9,7 +9,7 @@ int cardScores[] = {0,1,2,3,4,5,6,7,8,9,10,10,10,10};
 
 int drawCard(void){
 	//Write the function to random the number from 1 to 13 and return that random number.
-	int card = rand()%12 + 1;
+	int card = rand()%13 + 1;
 	return card;
 }
 
@@ -50,7 +50,7 @@ int main(){
 	
 	cout << "---------ORE NO TURN, DRAW!!!------------\n";
 	cout << "Your cards: " << cardNames[playerCards[0]] << " " << cardNames[playerCards[1]] << "\n";
-	playerScore = calScore(playerCards[0],playerCards[1],playerCards[2]);
+	playerScore = calScore(cardScores[playerCards[0]],cardScores[playerCards[1]],cardScores[playerCards[2]]);
 	cout << "Your score: " << playerScore << "\n";
 	do{
 		cout << "(1) Destiny draw (2) Stay, SELECT: ";
@@ -61,7 +61,7 @@ int main(){
 		playerCards[2] = drawCard();
 		cout << "Player draws the 3rd card!!!" << "\n";
 		cout << "Your 3rd card: " << cardNames[playerCards[2]] << "\n";
-		playerScore = calScore(playerCards[0],playerCards[1],playerCards[2]);
+		playerScore = calScore(cardScores[playerCards[0]],cardScores[playerCards[1]],cardScores[playerCards[2]]);
 		cout << "Your new score: " << playerScore << "\n";
 		
 	}
@@ -71,14 +71,14 @@ int main(){
 	
 	cout << "---------YUGI'S TURN, DRAW!!!------------\n";
 	cout << "Yugi's cards: " << cardNames[yugiCards[0]] << " " << cardNames[yugiCards[1]]<< "\n";
-	yugiScore = calScore(yugiCards[0],yugiCards[1],yugiCards[2]);
+	yugiScore = calScore(cardScores[yugiCards[0]],cardScores[yugiCards[1]],cardScores[yugiCards[2]]);
 	cout << "Yugi's score: " << yugiScore << "\n";
 	yugiAction = findYugiAction(yugiScore);
 	if(yugiAction == 1){
 		yugiCards[2] = drawCard();
 		cout << "Yugi draws the 3rd card!!!\n";
 		cout << "Yugi's 3rd card: "<< cardNames[yugiCards[2]] << "\n";
-		yugiScore = calScore(yugiCards[0],yugiCards[1],yugiCards[2]);
+		yugiScore = calScore(cardScores[yugiCards[0]],cardScores[yugiCards[1]],cardScores[yugiCards[2]]);
 		cout << "Yugi's new score: "<< yugiScore << "\n";
 	}
 	cout << "------------ Turn end -------------------\n";
